694d581bcd6328ccc23d2b0ddd5be8e130ca83ce
Ben <ben.bosman@student.hu.nl>
2026-09-23 11:59:41 +0200

    Made this a clean boiler plate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import SimpleVueComponents from 'simple-vue-components'
 import 'simple-vue-components/style.css'
 
-createApp(App).use(router).use(SimpleVueComponents).mount('#app')
+createApp(App).use(SimpleVueComponents).mount('#app')
